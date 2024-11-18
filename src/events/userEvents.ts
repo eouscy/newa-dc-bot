af2f623e851ad1dbb26b59ff7abc0f494d913c33
eouscy <eouscy@gmail.com>
2024-11-18 04:53:44 +0300

    Update userEvents with welcome message only

--- src/events/userEvents.ts
+++ src/events/userEvents.ts
@@ -4,15 +4,6 @@ import { getChannelFromChannelName } from '../utils';
 export const registerUserEvents = async (client: Client) => {
   client.on(Events.GuildMemberAdd, async (member: GuildMember) => {
     const systemChannel = member.guild.systemChannel;
-    const rulesChannel = await getChannelFromChannelName(member, 'guild-rules');
-    const accessChannel = await getChannelFromChannelName(
-      member,
-      'server-access'
-    );
-    const requestInviteChannel = await getChannelFromChannelName(
-      member,
-      'request-invite'
-    );
 
     const greeting = `Hello there ${member}, Welcome to ${member.guild.name}`;
 
